f27aff551a28b36e037ed42c0b3f514273179e75
unknown <huber.tom@swissonline.ch>
2014-12-06 14:57:10 +0100

    - config

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://niedersimmentaler:tvspiez@ds057000.mongolab.com:57000/niedersimmentaler',
-//  db: 'mongodb://localhost/mean-dev',
+  db: 'mongodb://localhost/mean-dev',
   app: {
     name: 'Dr Schnällscht Niedersimmentaler'
   },
